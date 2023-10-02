a4284c574a1dd1cf613ece275129a87a72497321
alexismaz <alexisfajian@hotmail.com>
2023-10-02 00:52:44 -0300

    dataChanges

--- lib/data.ts
+++ lib/data.ts
@@ -35,28 +35,28 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "HyperEvents",
+    location: "Buenos Aires, Arg",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "I work as a full-stack developer on this project. It's the final project of the bootcamp. I'm currently updating this project frequently.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "Sep 2023 - Present",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Graduated SoyHenry bootcamp",
+    location: "Buenos Aires, Arg",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+      "I graduated after 4 months of studying. I graduated as a full-stack web developer.",
+    icon: React.createElement(LuGraduationCap),
+    date: "May 2023 - Sep 2023",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Manager at the CASI club restaurant",
+    location: "San Isidro, Buenos Aires",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "I worked as a restaurant manager. I had approximately 20 employees under my supervision.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "Nov 2018, Nov 2022",
   },
 ] as const;
 
